e6bfd1296ca8a4620441f0a38c83ba6654586903
jonrene <jrjonathanrene@gmail.com>
2023-04-29 09:33:37 -0400

    Added Infinite Scrolling
    Added infinite scrolling to page. When user reaches bottom of page, more Video components are added to video grid.

--- src/components/VideoGrid.js
+++ src/components/VideoGrid.js
@@ -1,58 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Video from "./Video.js";
 import videoData from "../data/videoData.js";
 
 export default function VideoGrid(){
+
+    const [videoList, setVideoList] = useState([]);
+
+    useEffect(()=>{
+        let newVideoList = videoData.map((video, index)=>{
+            return(
+                <Video 
+                    key={index}
+                    thumbnail={video.thumbnail}
+                    channelImage={video.channelImage}
+                    videoTitle={video.videoTitle}
+                    channelName={video.channelName}
+                    views={video.views}
+                    date={video.date}
+                    length={video.length}
+                    >
+                </Video>
+            )
+        })
+        setVideoList(newVideoList);
+
+    },[])
+
+    useEffect(()=>{
+        window.onscroll = function(){
+            if((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight){
+                let currentVideos = videoList;
+                let addedVideos = videoData.map((video, index)=>{
+                    return(
+                        <Video 
+                            key={videoList.length + index}
+                            thumbnail={video.thumbnail}
+                            channelImage={video.channelImage}
+                            videoTitle={video.videoTitle}
+                            channelName={video.channelName}
+                            views={video.views}
+                            date={video.date}
+                            length={video.length}
+                            >
+                        </Video>
+                    )
+                })
+                setVideoList([...currentVideos,...addedVideos]);
+            }
+        }
+    })
+
+
+
     return(
         <>
             <div className="video-grid">
-                {videoData.map((video, index)=>{
-                    return(
-                            <Video 
-                                key={index}
-                                thumbnail={video.thumbnail}
-                                channelImage={video.channelImage}
-                                videoTitle={video.videoTitle}
-                                channelName={video.channelName}
-                                views={video.views}
-                                date={video.date}
-                                length={video.length}
-                                >
-                            </Video>
-                        )
-                })}
-
-                {videoData.map((video, index)=>{
-                    return(
-                            <Video 
-                                key={index}
-                                thumbnail={video.thumbnail}
-                                channelImage={video.channelImage}
-                                videoTitle={video.videoTitle}
-                                channelName={video.channelName}
-                                views={video.views}
-                                date={video.date}
-                                length={video.length}
-                                >
-                            </Video>
-                        )
-                })}
-
-                {videoData.map((video, index)=>{
-                    return(
-                            <Video 
-                                key={index}
-                                thumbnail={video.thumbnail}
-                                channelImage={video.channelImage}
-                                videoTitle={video.videoTitle}
-                                channelName={video.channelName}
-                                views={video.views}
-                                date={video.date}
-                                length={video.length}
-                                >
-                            </Video>
-                        )
-                })}
+                {videoList}
             </div>
         </>
     )
--- src/data/videoData.js
+++ src/data/videoData.js
@@ -52,9 +52,62 @@ let videoData = [
         views: "6M views",
         date: "6 years ago",
         length: "13:45"
+    },
+    {
+        thumbnail: "./images/thumbnails/nutshell.webp",
+        channelImage: "./images/profiles/nutshell.jpeg",
+        videoTitle: "The Fermi Paradox - Where Are All The Aliens? (1/2)",
+        channelName: "Kurzgesagt – In a Nutshell",
+        views: "32M views",
+        date: "7 years ago",
+        length: "6:21"
+    },
+    {
+        thumbnail: "./images/thumbnails/beast.webp",
+        channelImage: "./images/profiles/beast.jpeg",
+        videoTitle: "$1 vs $500,000 Plane Ticket!",
+        channelName: "MrBeast",
+        views: "135M views",
+        date: "3 weeks ago",
+        length: "12:20"
+    },
+    {
+        thumbnail: "./images/thumbnails/markiplier.webp",
+        channelImage: "./images/profiles/markiplier.jpeg",
+        videoTitle: "Try Not To Laugh Challenge",
+        channelName: "Markiplier",
+        views: "41M views",
+        date: "6 years ago",
+        length: "7:45"
+    },
+    {
+        thumbnail: "./images/thumbnails/mkbhd.webp",
+        channelImage: "./images/profiles/mkbhd.jpeg",
+        videoTitle: "I Made My Own Sneaker! M251",
+        channelName: "Marques Brownlee",
+        views: "1M views",
+        date: "1 day ago",
+        length: "7:02"
+    },
+    {
+        thumbnail: "./images/thumbnails/vsauce.webp",
+        channelImage: "./images/profiles/vsauce.jpeg",
+        videoTitle: "Is Earth Actually Flat?",
+        channelName: "Vsauce",
+        views: "34M views",
+        date: "8 years ago",
+        length: "10:31"
+    },
+    {
+        thumbnail: "./images/thumbnails/philip.webp",
+        channelImage: "./images/profiles/philip.jpeg",
+        videoTitle: "WOW... We Need To Talk About This...",
+        channelName: "Philip DeFranco",
+        views: "6M views",
+        date: "6 years ago",
+        length: "13:45"
     }
     
-
 ]
 
 export default videoData;
