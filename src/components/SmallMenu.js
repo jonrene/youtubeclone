1bc0f5a8647b3377bb0641e8df9d42abfc2ba3e9
jonrene <jrjonathanrene@gmail.com>
2023-05-22 18:48:37 -0400

    updated SmallMenuButton component
    added missing alt props for smallMenuButton Component

--- src/components/SmallMenu.js
+++ src/components/SmallMenu.js
@@ -9,10 +9,10 @@ export default function SmallMenu(){
     return(
         <>
            <div className="small-menu">
-                <SmallMenuNavButton imgSrc={"./images/menu/home.svg"} description={"Home"}></SmallMenuNavButton>
-                <SmallMenuNavButton imgSrc={"./images/menu/shorts.svg"} description={"Shorts"}></SmallMenuNavButton>
-                <SmallMenuNavButton imgSrc={"./images/menu/subscriptions.svg"} description={"Subscriptions"}></SmallMenuNavButton>
-                <SmallMenuNavButton imgSrc={"./images/menu/library.svg"} description={"Library"}></SmallMenuNavButton>
+                <SmallMenuNavButton imgSrc={"./images/menu/home.svg"} description={"Home"} alt={'home button'}></SmallMenuNavButton>
+                <SmallMenuNavButton imgSrc={"./images/menu/shorts.svg"} description={"Shorts"} alt={'shorts button'}></SmallMenuNavButton>
+                <SmallMenuNavButton imgSrc={"./images/menu/subscriptions.svg"} description={"Subscriptions"} alt={'subscriptions button'}></SmallMenuNavButton>
+                <SmallMenuNavButton imgSrc={"./images/menu/library.svg"} description={"Library"} alt={'library button'}></SmallMenuNavButton>
            </div>
         </>
     )
